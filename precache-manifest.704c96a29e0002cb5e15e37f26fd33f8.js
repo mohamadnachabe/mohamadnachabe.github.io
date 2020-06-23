e8179a2b6591b80146c308e278f73467da3aa0de
Mohamad Nachabe <mohamadnachabe@gmail.com>
2020-06-23 21:42:41 +0100

    Updates

---
+++ precache-manifest.704c96a29e0002cb5e15e37f26fd33f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08966dfb38d3a34878f7ed8c93247fc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "552f90e6750beb9b600e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cfbeba24898ae65e85ca",
+    "url": "/static/js/2.cc26475d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc26475d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "552f90e6750beb9b600e",
+    "url": "/static/js/main.bbffca7e.chunk.js"
+  },
+  {
+    "revision": "064c513b534cb055f940",
+    "url": "/static/js/runtime-main.df3c7cab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
