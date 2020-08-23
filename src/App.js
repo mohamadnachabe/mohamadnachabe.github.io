3020c3e7d4d8517e94e4b25e93af7d4a18f4e173
Mohamad Nachabe <mohamadnachabe@gmail.com>
2020-08-23 14:23:34 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          mohamadnachabe
+          mohamad nachabe
         </p>
         <a href="https://github.com/mohamadnachabe">
           <img width="20" height="20" src={require('./github.svg')} />
